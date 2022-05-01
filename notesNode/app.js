7c2605bac11fcc80084338f609b66bc323fbfb15
aayushkabra1 <ayushkabra1212@gmail.com>
2022-05-01 21:43:38 +0530

    installig third party modules

--- notesNode/app.js
+++ notesNode/app.js
@@ -2,6 +2,14 @@ console.log("starting app...");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 var user = os.userInfo();
+
+// console.log(_.isString(1));
+// console.log(_.isString("a"));
+
+var arr = _.uniq([1, 1, 1, 2, 2, 3, 4, 5,5]);
+console.log(arr);
+
 // fs.appendFileSync('hello.txt', "Hello World!\n");
-fs.appendFileSync('hello.txt', "Hello " + user.username + "!\n");
+// fs.appendFileSync('hello.txt', "Hello " + user.username + "!\n");
